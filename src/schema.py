6e45f3f28b9257ad95f99b58d6abd76d14bf922a
kevinrobles1 <kevinrobles1@arizona.edu>
2026-02-13 15:17:46 -0700

    Create schema.py

---
+++ src/schema.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class StandardColumns:
+    site_id: str = "site_id"
+    operator: str = "operator"
+    name: str = "name"
+    lat: str = "lat"
+    lon: str = "lon"
+    datum: str = "datum"
+    status: str = "status"
+    start_date: str = "start_date"
+    end_date: str = "end_date"
+    url: str = "url"
+
+STD = StandardColumns()
+
+MASTER_COL_ORDER = [
+    STD.site_id,
+    STD.operator,
+    STD.name,
+    STD.lat,
+    STD.lon,
+    STD.datum,
+    STD.status,
+    STD.start_date,
+    STD.end_date,
+    STD.url,
+]
